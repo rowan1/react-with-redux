45f4f123c3fca223b68d97e7791b3df3315787fb
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2020-04-12 22:05:51 +0200

    created redux actions and reducers

---
+++ react-with-redux/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App';
+import reducers from './reducers';
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>
+    
+    , document.querySelector('#root')
+);
