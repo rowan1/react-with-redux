60fc65669ad0b95bc5c69a08fa40e73f712c47c5
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2020-04-14 01:04:13 +0200

    another colution for over fetching

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
 
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     };
     renderList(){
         return this.props.posts.map(post => {
@@ -28,7 +28,6 @@ class PostList extends React.Component {
     }
 
     render(){
-        console.log(this.props.posts);
         return(
             <div className="ui relaxed divided list">
                 {this.renderList()}
@@ -41,4 +40,4 @@ const mapStateToProps =(state)=> {
     return {posts: state.posts}
 }
 
-export default connect(mapStateToProps, {fetchPosts})(PostList);
+export default connect(mapStateToProps, {fetchPostsAndUsers})(PostList);
